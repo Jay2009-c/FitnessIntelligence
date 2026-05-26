a82e7e5a49fc9e10c1b774ab5ebd4122843a1655
Jay2009-c <jaymhatre99990@gmail.com>
2026-05-26 07:59:23 +0530

    Delete src/main directory

--- src/main/java/com/example/fitnessanalyzer/domain/model/UserProfile.kt
+++
@@ -1,8 +0,0 @@
-package com.example.fitnessanalyzer.domain.model
-
-data class UserProfile(
-    val age: Int? = null,
-    val weightKg: Float? = null,
-    val heightCm: Float? = null,
-    val isMale: Boolean? = null
-)
